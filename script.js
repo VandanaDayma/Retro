61be544f1c56f22e683b7bcd50ba60f09447b92f
VandanaDayma <141750393+VandanaDayma@users.noreply.github.com>
2024-05-25 19:51:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('.main'),
+//     smooth: true
+// });
+// var tl = gsap.timeline()
+
+// tl.to(".page1",{
+//     y:"100vh",
+//     scale:0.6,
+//     duration:0
+// })
+
+// tl.to(".page1",{
+// y:"30vh",
+// duration:1,
+// delay:1})
+
+// tl.to(".page1",{
+//     y:"0vh",
+//     rotate:360,
+//     scale:1,
+//     duration:0.7
+// })
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+var tl = gsap.timeline()
+
+tl.to(".main1",{
+    y:"100vh",
+    scale:0.6,
+    duration:0
+})
+
+tl.to(".main1",{
+y:"30vh",
+duration:3,
+delay:1})
+
+tl.to(".main1",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration:2
+})
